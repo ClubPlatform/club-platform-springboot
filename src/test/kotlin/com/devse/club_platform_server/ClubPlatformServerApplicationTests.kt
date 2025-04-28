4c82f5601de915b1bdf42414181a9cddaa891529
jsum01 <djadjawltjr@gmail.com>
2025-04-28 10:34:52 +0900

    test: Add basic test class for application startup

---
+++ src/test/kotlin/com/devse/club_platform_server/ClubPlatformServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devse.club_platform_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClubPlatformServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
