8cf2d745336ce5eadbcc0b8858de5c9a72d70131
jsum01 <djadjawltjr@gmail.com>
2025-04-28 10:34:44 +0900

    feat: Add main application entry point

---
+++ src/main/kotlin/com/devse/club_platform_server/ClubPlatformServerApplication.kt
@@ -0,0 +1,11 @@
+package com.devse.club_platform_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClubPlatformServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClubPlatformServerApplication>(*args)
+}
